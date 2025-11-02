5eadcfe1fdf30e191ef9c678df8499fc64efca4b
Duy Trường <duytruongk22@gmail.com>
2025-11-02 22:16:58 +0700

    Initial project setup and database connection

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+const { testConnection } = require('./config/db');
+
+// Lấy PORT từ file .env, nếu không có thì mặc định là 3000
+const PORT = process.env.PORT || 3306;
+
+// Hàm khởi động server
+const startServer = async () => {
+    // 1. Kiểm tra kết nối database
+    await testConnection();
+
+    // 2. Khởi động server
+    app.listen(PORT, () => {
+        console.log(`🚀 Server is running on port ${PORT}`);
+        console.log(`Access at: http://localhost:${PORT}`);
+    });
+};
+
+// Chạy server
+startServer();
