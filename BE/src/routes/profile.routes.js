ac231483188c4cc8976f86bb271884080797e984
unknown <vuminhduc27052004@gmail.com>
2025-11-18 12:37:45 +0700

    sửa avater user

--- BE/src/routes/profile.routes.js
+++ BE/src/routes/profile.routes.js
@@ -51,20 +51,21 @@ const uploadCloud = require('../config/cloudinary');
  */
 router.get('/', authMiddleware, async (req, res) => {
     try {
-        // req.user được gán từ authMiddleware
         const user = await userModel.findById(req.user.userId);
         if (!user) {
             return res.status(404).json({ message: 'Không tìm thấy người dùng.' });
         }
-        // Chỉ trả về thông tin an toàn
+
+        // Trả về thêm avatar_url (có thể là null)
         res.status(200).json({
             userId: user.user_id,
             email: user.email,
             fullName: user.full_name,
-            provider: user.provider, // Rất quan trọng (local, google...)
-            role: user.role, // Thêm role vào response
-            avatar_url: user.avatar_url || null
+            provider: user.provider,
+            role: user.role,
+            avatar_url: user.avatar_url 
         });
+
     } catch (error) {
         res.status(500).json({ message: 'Lỗi máy chủ', error: error.message });
     }
